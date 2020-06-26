eaf97614a9e3f6690f71338cb7a9ce511ea386e6
Michael Arnold Owens <xmichael.owens@gmail.com>
2020-06-26 12:24:44 -0700

    d3 types draw a box with d3

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,5 +1,8 @@
+import * as d3 from 'd3';
+
+import React, { useEffect, useState } from 'react';
+
 import { ChartData } from '../types/ChartTypes';
-import React from 'react';
 
 interface ChartProps {
   data: Array<ChartData>;
@@ -8,12 +11,34 @@ interface ChartProps {
   color?: string;
 }
 
+function drawChart(height, width){
+  d3.select("#chart")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .style("border", "1px solid black")
+      .append("text")
+      .attr("fill", "green")
+      .attr("x", 50)
+      .attr("y", 50)
+      .text("Hello D3")
+}
+
 function Chart(props: ChartProps) {
-  return (
-    <div>
-      Chart Goes here
-    </div>
-  )
+  const [data, setData] = useState([]);
+
+    useEffect(() => {
+        drawChart(400, 600);
+    }, []);
+
+
+    return (
+        <div className="App">
+            <h2>Graphs with React</h2>
+            <div id="chart">
+            </div>
+        </div>
+    );
 }
 
 export default Chart;
